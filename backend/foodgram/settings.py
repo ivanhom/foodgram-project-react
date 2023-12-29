adfe6f449c3987a2af0787dd787edb636c25cc1d
Иван Хомюк <mr.homyuk@mail.ru>
2023-12-29 14:03:02 +0900

    Media dont work.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,12 +7,17 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG_MODE', 'True')
+# DEBUG = os.getenv('DEBUG_MODE', 'True')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
 
+SECRET_KEY = 'django-insecure-h1!z0yowa)bi0xzigqo&+4olij!jnx@wj#h0_e4kosd@qfp^1p'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['158.160.23.164', '127.0.0.1', 'localhost', 'tregalex.ddns.net']
 
 # Application definition
 
